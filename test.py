b1ef0fa1d3618a3ee5dbb3c286e2542e84bb30ae
Сергей <tyapkin2002@mail.ru>
2021-11-20 00:49:22 +0300

    Тесты запускаются по http порту, а там уже переназначаются

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-base_path = "http://android-task-tracker-backend.herokuapp.com:19908"
+base_path = "http://android-task-tracker-backend.herokuapp.com"
 session = requests.Session()
 
 requests_total = 0
