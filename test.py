ba0cd9f101493cb3b7b6ab06b36a4d1c5cb64c1f
Сергей <tyapkin2002@mail.ru>
2021-11-19 22:26:40 +0300

    Вернул порт обратно)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-base_path = "http://android-task-tracker-backend.herokuapp.com:80"
+base_path = "http://android-task-tracker-backend.herokuapp.com:9000"
 session = requests.Session()
 
 requests_total = 0
